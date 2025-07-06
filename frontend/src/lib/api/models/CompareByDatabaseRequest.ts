d7df61bb8a861f4b9fc4fe1d7070c13778a7e090
trankhanh040147 <trankhanh040147@gmail.com>
2025-07-06 16:38:10 +0700

    feat: remove frontend

--- frontend/src/lib/api/models/CompareByDatabaseRequest.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type CompareByDatabaseRequest = {
-    source_database_id: string;
-    target_database_id: string;
-};
-
--- frontend/src/lib/api/models/IndexUpdate.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type IndexUpdate = {
-    name?: string;
-    unique?: boolean;
-};
-
